22eaaadd642982dfa3b2a6895541450625ac9927
danielmarbach <daniel.marbach@openplace.net>
2015-11-16 21:09:45 +0100

    Basic test infrastructure

---
+++ Facility.Producer/Handler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using Messages;
+using NServiceBus;
+
+namespace Facility.Producer
+{
+    public class Handler : IHandleMessages<ProduceChocolateBar>
+    {
+        public void Handle(ProduceChocolateBar message)
+        {
+            Thread.Sleep(100);
+            Syncher.SyncEvent.Signal();
+        }
+    }
+}
---
+++ Facility/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace Facility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            DefaultFactory defaultFactory = LogManager.Use<DefaultFactory>();
+            defaultFactory.Level(LogLevel.Error);
+
+            var configuration = new BusConfiguration();
+            configuration.EndpointName("Chocolate.Facility");
+
+            configuration.UseTransport<MsmqTransport>();
+            configuration.UsePersistence<InMemoryPersistence>();
+
+            var bus = Bus.Create(configuration).Start();
+            stopWatch.Stop();
+
+            Console.WriteLine($"Initalizing the bus took { stopWatch.Elapsed.ToString("G")}");
+            stopWatch.Reset();
+
+            var destination = "Chocolate.Facility.Producer";
+            stopWatch.Start();
+            Parallel.For(0, Constants.NumberOfMessages, new ParallelOptions(), i =>
+            {
+                bus.Send(destination, new ProduceChocolateBar { LotNumber = i, MaxLotNumber = Constants.NumberOfMessages });
+            });
+            stopWatch.Stop();
+            Console.WriteLine($"Sending #{ Constants.NumberOfMessages } of msgs over the bus took { stopWatch.Elapsed.ToString("G")}");
+
+            Console.ReadLine();
+        }
+    }
+}
